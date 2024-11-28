396f986ecd9d1f3a83fb76f0bd5e2a3d125d7398
Ben Chatelain <ben@octop.ad>
2024-11-28 09:44:21 -0700

    ☕️ Add src:LibraryExample target, use in HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        IntMultiplier im = new IntMultiplier(3, 4);
+        System.out.println(im.getProduct());
     }
 }
---
+++ src/IntMultiplier.java
@@ -0,0 +1,11 @@
+public class IntMultiplier {
+    private int a;
+    private int b;
+    public IntMultiplier(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int getProduct() {
+        return a * b;
+    }
+}
