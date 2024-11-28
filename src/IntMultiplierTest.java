4c3cbba5886ee5eaffd81de243e5b88134ed6c92
Ben Chatelain <ben@octop.ad>
2024-11-28 10:29:50 -0700

    ✅ Add another tst

--- src/IntMultiplierTest.java
+++ src/IntMultiplierTest.java
@@ -6,4 +6,9 @@ public class IntMultiplierTest {
         IntMultiplier im = new IntMultiplier(3, 4);
         assertEquals(12, im.getProduct());
     }
+    @Test
+    public void testIntMultiplierAgain() throws Exception {
+        IntMultiplier im = new IntMultiplier(4, 5);
+        assertEquals(20, im.getProduct());
+    }
 }
