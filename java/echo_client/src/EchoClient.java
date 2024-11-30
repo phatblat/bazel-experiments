3d3cc679c8ef19c2416fb2148f3a347c5a914bde
Ben Chatelain <ben@octop.ad>
2024-11-30 08:21:26 -0700

    ♻️ Use transmission_object_java_proto lib in echo_client

--- java/echo_client/src/EchoClient.java
+++ java/echo_client/src/EchoClient.java
@@ -4,14 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import transmission_object.TransmissionObjectOuterClass.TransmissionObject;
 
 public class EchoClient {
     public static void main(String[] args) {
         System.out.println("Spinning up Echo Client in Java...");
         try {
             final Socket socketToServer = new Socket("localhost", 1234);
-            final BufferedReader inputFromServer = new BufferedReader(
-                new InputStreamReader(socketToServer.getInputStream()));
             final BufferedReader commandLineInput = new BufferedReader(
                 new InputStreamReader(System.in));
 
@@ -19,16 +18,17 @@ public class EchoClient {
             final String inputFromUser = commandLineInput.readLine();
             if (inputFromUser != null)  {
                 System.out.println("Received by Java: " + inputFromUser);
-                TransmissionObject transmissionObject = new TransmissionObject();
-                transmissionObject.message = inputFromUser;
-                transmissionObject.value = 3.14f;
-                GsonBuilder builder = new GsonBuilder();
-                Gson gson = builder.create();
-                final PrintWriter outputToServer =
-                    new PrintWriter(socketToServer.getOutputStream(), true);
-                outputToServer.println(gson.toJson(transmissionObject));
-                System.out.println(inputFromServer.readLine());
+                TransmissionObject transmissionObject = TransmissionObject
+                    .newBuilder()
+                    .setMessage(inputFromUser)
+                    .setValue(3.145f)
+                    .build();
+                transmissionObject.writeTo(socketToServer.getOutputStream());
+                TransmissionObject receivedObject =
+                    TransmissionObject.parseFrom(socketToServer.getInputStream());
+                System.out.println("Received Message from server: " + receivedObject);
             }
+
             socketToServer.close();
         } catch (Exception e) {
             System.out.println("Error: " + e);
